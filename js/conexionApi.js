1435302baf1da56d8a8bf5e34bf75b3973ba6da4
Evelyn Ortiz <evelyn.ortiz.hu@gmail.com>
2024-12-11 10:53:33 -0500

    update

--- js/conexionApi.js
+++ js/conexionApi.js
@@ -1,10 +1,41 @@
-async function listarVideos() {
-  const conexion = await fetch("https://8zk6kp-3001.csb.app/videos");
-  const conexionConvertida = conexion.json();
-
+async function listaVideos(){
+  const conexion = await fetch("http://localhost:3001/videos"
+  //   ,{
+  //     method:"GET",
+  //     headers:{
+  //     "Content-type":"application/json",
+  //     "Permissions-Policy": "geolocation=(self `http://localhost:3001/videos`)"
+  //     }
+  // }
+);
+  
+  const conexionConvertida=await conexion.json();
+  // //res.setHeader('Permissions-Policy', 'ch-ua-form-factor');
+  // console.log(conexion);
+  // console.log(conexionConvertida);
   return conexionConvertida;
 }
 
-export const conexionApi = {
-  listarVideos,
-};
+// async function crearVideo(titulo,descripcion,url,imagen){
+//   const conexion= await fetch("http://localhost:3001/videos",{
+//   method:"POST",
+//   headers:{
+//       "Content-type":"'application/json; charset=utf-8'"
+//   },
+//   body:JSON.stringify({
+//       titulo:titulo,
+//       descripcion:`${descripcion} mil visualizaciones`,
+//       url:url,
+//       imagen:imagen
+//   })
+//   })
+
+//   const conexionConvertida = await conexion.json();
+
+//   return conexionConvertida;
+// }
+
+export const conectaAPI={
+  listaVideos
+  // ,crearVideo
+}
